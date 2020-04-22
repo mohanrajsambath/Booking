62bdcf0e58b31294541ff1111e16c99764357722
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2020-04-22 15:53:39 +0530

    Design Changes done
    Signed-off-by: MohanrajS19 <mohanraj.sambath@ibotsystems.com>

--- app/src/main/java/mohan/com/booking/MainActivity.kt
+++ app/src/main/java/mohan/com/booking/MainActivity.kt
@@ -1,18 +1,13 @@
 package mohan.com.booking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import mohan.com.booking.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        setContentView(R.layout.home_item_layout)
+
     }
 }
